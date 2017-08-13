1e7f5429f2182f83bdaa59b3bcd8ee113840bac6
JeongEuiJin <hehar1020@gmail.com>
2017-08-14 01:39:00 +0900

    member API 구현 , eb init중

---
+++ django-app/member/apis.py
@@ -0,0 +1,153 @@
+import requests
+from django.conf import settings
+from django.contrib.auth import authenticate, get_user_model
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
+from rest_framework import permissions, generics, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import authentication_classes
+from rest_framework.exceptions import APIException
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import logout as django_logout
+from member.permissions import ObjectIsRequestUser
+from django.utils.translation import ugettext_lazy as _
+
+from .serializers import UserSerializer, UserCreationSerializer, UserLoginSerializer
+
+User = get_user_model()
+
+class TokenUserInfoAPIView(APIView):
+    def post(self, request):
+        token_string = request.data.get('token')
+        try:
+            token = Token.objects.get(key=token_string)
+        except Token.DoesNotExist:
+            raise APIException('token invalid')
+        user = token.user
+
+        return Response(UserSerializer(user).data)
+
+class UserLogin(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serial_data = UserLoginSerializer(request.data)
+        plot_user = authenticate(email=serial_data.data["email"],
+                                 password=serial_data.data["password"])
+        if plot_user:
+            token = Token.objects.get_or_create(user=plot_user)[0]
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
+        return Response({"error": "아이디 혹은 비밀번호가 올바르지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogout(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        return self.logout(request)
+
+    def logout(self, request):
+        try:
+            request.user.auth_token.delete()
+        except (AttributeError, ObjectDoesNotExist):
+            return Response({"detail": _("토큰이 제공되지 않았습니다.")},
+                            status=status.HTTP_401_UNAUTHORIZED)
+
+        django_logout(request)
+
+        return Response({"detail": _("로그아웃 되었습니다.")},
+                        status=status.HTTP_200_OK)
+
+class UserSignUp(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserCreationSerializer
+
+
+
+
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return UserSerializer
+        elif self.request.method == 'POST':
+            return UserCreationSerializer
+
+
+class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (
+        permissions.IsAuthenticated,
+        ObjectIsRequestUser,
+    )
+
+class UserDetailView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, pk=None):
+        return Response(UserSerializer(request.user).data)
+
+
+class FacebookLoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+
+    APP_ACCESS_TOKEN = '{}|{}'.format(
+        settings.FACEBOOK_APP_ID,
+        settings.FACEBOOK_SECRET_CODE,
+    )
+
+    def post(self, request):
+        token = request.data.get('token')
+        if not token:
+            raise APIException('token require')
+
+        self.debug_token(token)
+        user_info = self.get_user_info(token=token)
+        if User.objects.filter(username=user_info['id']).exists():
+            user = User.objects.get(username=user_info['id'])
+        else:
+            user = User.objects.create_facebook_user(user_info)
+
+        token, token_created = Token.objects.get_or_create(user=user)
+        ret = {
+            'token': token.key,
+            'user': UserSerializer(user).data,
+        }
+        return Response(ret)
+
+    def debug_token(self, token):
+        url_debug_token = 'https://graph.facebook.com/debug_token'
+        url_debug_token_params = {
+            'input_token': token,
+            'access_token': self.APP_ACCESS_TOKEN
+        }
+        response = requests.get(url_debug_token, url_debug_token_params)
+        result = response.json()
+        if 'error' in result or 'error' in result['data']:
+            raise APIException('token invalid')
+        else:
+            return result
+
+    def get_user_info(self, token):
+        url_user_info = 'https://graph.facebook.com/v2.9/me'
+        url_user_info_params = {
+            'access_token': token,
+            'fields': ','.join([
+                'id',
+                'name',
+                'email',
+                'first_name',
+                'last_name',
+                'picture.type(large)',
+                'gender',
+            ])
+        }
+        response = requests.get(url_user_info, params=url_user_info_params)
+        result = response.json()
+        return result
---
+++ django-app/member/models.py
@@ -0,0 +1,101 @@
+from django.conf import settings
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+    def create_facebook_user(self, user_info):
+        self.create_user(
+            email='{}@facebook.com'.format(user_info['id']),
+            username=user_info['id'],
+            nickname=user_info['id'],
+            first_name=user_info.get('first_name', ''),
+            last_name=user_info.get('last_name', ''),
+            img_profile=user_info['picture']['data']['url'],
+            user_type=User.USER_TYPE_FACEBOOK
+        )
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    USER_TYPE_DJANGO = 'django'
+    USER_TYPE_FACEBOOK = 'facebook'
+    CHOICES_USER_TYPE = (
+        (USER_TYPE_DJANGO, 'Django'),
+        (USER_TYPE_FACEBOOK, 'Facebook'),
+    )
+    username = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20)
+    email = models.EmailField(_('email address'), unique=True)
+    img_profile = models.ImageField(upload_to='user/', null=True, blank=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+
+    user_type = models.CharField(max_length=20, choices=CHOICES_USER_TYPE, default=USER_TYPE_DJANGO)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        return self.nickname
+
+    def get_short_name(self):
+        return self.nickname
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
