373faefa8310d5b6b8fb9c42303f1aff16f78ffb
JeongEuiJin <hehar1020@gmail.com>
2017-08-18 18:54:05 +0900

    지역 강원도 추가

--- django-app/post/models.py
+++ django-app/post/models.py
@@ -28,6 +28,7 @@ class Post(models.Model):
         ('ulsan', '울산'),
         ('gwangju', '광주'),
         ('gyeonggi','경기도'),
+        ('gangwon','강원도'),
         ('chungcheonnam', '충청남도'),
         ('chungcheonbuk', '충청북도'),
         ('gyeongsangnam', '경상남도'),
